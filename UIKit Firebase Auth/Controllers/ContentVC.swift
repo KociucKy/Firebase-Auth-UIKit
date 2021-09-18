c7574f2edce0df09428824f94a83dce51ecc4939
Jakub Kociucki <jakubkociucki.dev@gmail.com>
2021-09-18 14:58:12 +0200

    Implemented logging out functionality in the ContentVC

--- UIKit Firebase Auth/Controllers/ContentVC.swift
+++ UIKit Firebase Auth/Controllers/ContentVC.swift
@@ -1,8 +1,22 @@
 import UIKit
+import Firebase
 
 class ContentVC: UIViewController{
     
+    //MARK: - VC Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+    }
+    
+    //MARK: - Actions
+    @IBAction func logOutPressed(_ sender: UIButton) {
+        let firebaseAuth = Auth.auth()
+        do{
+            try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+        }catch let signOutError as NSError{
+            print(signOutError.localizedDescription)
+        }
     }
 }
